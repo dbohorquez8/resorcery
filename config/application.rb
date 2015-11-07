e71f6755588d65a099ac76647b5b57742af76742
lacides <lcharris89@gmail.com>
2015-11-07 16:01:36 -0500

    Add sendgrid config

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,15 @@ module Resorcery
     config.active_record.raise_in_transactional_callbacks = true
 
     config.action_mailer.default_url_options = { host: ENV['APPLICATION_HOST'] }
+
+    config.action_mailer.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+    }
   end
 end
