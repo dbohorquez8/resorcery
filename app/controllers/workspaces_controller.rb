5ae0f3f3aa7e5cbbbe14d4093112448c8e4c6fea
Jose Felix Gomez <jose.gomez@koombea.com>
2015-11-06 21:20:15 -0500

    Added workspaces controller and default route to Workspaces#index

---
+++ app/controllers/workspaces_controller.rb
@@ -0,0 +1,8 @@
+class WorkspacesController < ApplicationController
+  def index
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+end
---
+++ spec/routing/workspaces_routing_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe "routing to workspaces", :type => :routing do
+  it "routes / to workspaces#index" do
+    expect(get: "/").to route_to( controller: "workspaces", action: "index")
+  end
+end
+
