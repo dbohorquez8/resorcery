4bcf8d51a311789a2d9f997a8e608d84de279abb
Miguel Perez <miguel.perez@koombea.com>
2015-11-08 09:57:52 -0500

    adding basic statistics

--- app/controllers/api/v1/workspaces_controller.rb
+++ app/controllers/api/v1/workspaces_controller.rb
@@ -17,10 +17,27 @@ class Api::V1::WorkspacesController < ApiController
     workspace = current_user.workspaces.from_unique_id(workspace_id).includes(:resources, :resource_groups).first!
     serialized = WorkspaceSerializer.new(workspace).serializable_hash
     serialized[:allocations] = ActiveModel::ArraySerializer.new(workspace.allocations.in_range(@start_date, @end_date), each_serializer: AllocationSerializer).serializable_array
-    expose(serialized)
+    expose serialized, metadata: calculate_statistics(serialized)
   end
 
   private
+    def calculate_statistics(workspace)
+      allocated_resources = workspace[:allocations].map{|r| r[:resource_id]}.uniq.count
+      allocated_groups = workspace[:allocations].map{|r| r[:resource_group_id]}.uniq
+
+      groups_without_allocations = (workspace[:resource_groups].map{|r| r[:id]} - allocated_groups).count
+
+      total_resources = workspace[:resources].count
+      {
+        estatistics: {
+          percentage_of_resources_allocated: (allocated_resources*100)/total_resources,
+          allocated_resources: allocated_resources,
+          total_resources: total_resources,
+          groups_without_allocations: groups_without_allocations
+        }
+      }
+    end
+
     def workspace_params
       params.require(:workspace).permit(:name, metadata: [:resources_name, :resource_groups_name])
     end
