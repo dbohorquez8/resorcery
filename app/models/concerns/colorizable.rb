527091182887ffb76de2e65f96a472f38d8a80c7
Miguel Perez <miguel.perez@koombea.com>
2015-11-07 22:08:06 -0500

    fixing bug in colorizable

--- app/models/concerns/colorizable.rb
+++ app/models/concerns/colorizable.rb
@@ -7,6 +7,6 @@ module Colorizable
 
   def setup_color
     generator = ColorGenerator.new saturation: 0.3, lightness: 0.75
-    self.metadata[:background_color] = generator.create_hex
+    self.metadata['background_color'] = generator.create_hex
   end
 end
