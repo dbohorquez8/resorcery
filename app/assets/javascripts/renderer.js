6aa7590cbda4ea938701e230012818af994f300f
dbohorquez8 <dbohorquez8@gmail.com>
2015-11-07 21:04:05 -0500

    Add group title to chart

--- app/assets/javascripts/renderer.js
+++ app/assets/javascripts/renderer.js
@@ -25,35 +25,34 @@ $(function (){
     margin = 5,
     radius;
 
-    function maxResourcesPerRingIndex(ringIndex) {
-      return 3 * ringIndex * (ringIndex + 1);
-    }
+      $('.js-workspace-chart-group').each(function (){
+        var ringIndex = 1, maxResources = $(this).find('.js-workspace-chart-resource').length + 1;
 
-    $('.js-workspace-chart-group').each(function (){
-      var ringIndex = 1, maxResources = $(this).find('.js-workspace-chart-resource').length;
+        $(this).find('.js-workspace-chart-resource').each(function (index){
+          var index = index + 1;
+          var resource = $(this);
+          var delta = maxResourcesPerRingIndex(ringIndex) > maxResources - 1 ? (maxResources - 1) - maxResourcesPerRingIndex(ringIndex - 1) : 6 * ringIndex;
 
-      $(this).find('.js-workspace-chart-resource').each(function (index){
-        var resource = $(this);
-        var delta = maxResourcesPerRingIndex(ringIndex) > maxResources - 1 ? (maxResources - 1) - maxResourcesPerRingIndex(ringIndex - 1) : 6 * ringIndex;
-
-        if(index == 0){
-          resource.css('top', 0 - resourceSize / 2)
-          .css('left', 0 - resourceSize / 2);
-          radius = (resourceSize) / 2;
-        }else{
           radius = (resourceSize + margin) * (ringIndex);
           var angle = 360 / delta;
 
           if(index == maxResourcesPerRingIndex(ringIndex)){
-            ringIndex++;
+            ringIndex++;           
           }
         }
 
-        var x = 0 - resourceSize / 2 - radius * Math.sin(angle * index * Math.PI / 180);
-        var y = 0 - resourceSize / 2 - radius * Math.cos(angle * index * Math.PI / 180);
-        resource.css('top', y)
-        .css('left', x);
-      });
+
+          var x = 0 - resourceSize / 2 - radius * Math.sin(angle * index * Math.PI / 180);
+          var y = 0 - resourceSize / 2 - radius * Math.cos(angle * index * Math.PI / 180);
+          resource.css('top', y)
+          .css('left', x);
+        });
+
+        var size = 2 * radius + resourceSize;
+        $(this).width(size);
+        $(this).height(size);
+        $(this).find('.js-workspace-chart-resource, .js-group-title').css('transform', 'translate('+ size / 2 +'px, '+ size / 2 +'px)');
+    });
 
       var size = 2 * radius + resourceSize;
       $(this).width(size);
