d4cc2a38ed6c9249a5e7735057d47086a3a53860
Miguel Perez <miguel.perez@koombea.com>
2015-11-08 17:01:13 -0500

    fixing bug in landing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,9 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( landing.css landing/animate-headlines.js assets/svg/logo-white.svg )
+Rails.application.config.assets.precompile += %w(
+  landing.js
+  landing.css
+  landing/animate-headlines.js
+  assets/svg/logo-white.svg
+)
